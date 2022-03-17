e53afe7e3e5a25f4da0ae53a73dab5ac9b8ab54d
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-03-17 19:56:16 +0100

    first component

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+// Decorator (brings modifications to the class)
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {}
