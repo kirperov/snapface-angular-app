615a85a2e178f10da3ea8cafe8bbbef5405dbb55
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-03-18 15:21:46 +0100

    Preparation data to show

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 // Decorator (brings modifications to the class)
 @Component({
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {}
+// OnInit implementé la methode
+export class FaceSnapComponent implements OnInit {
+  // Propriétés por les données
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  
+  // Appelée 1 fois par instance au moment de la création (Initialisation de données en dur mais possible du serveur ou API) 
+ /* La méthode  ngOnInit()  est appelée automatiquement par Angular
+  au moment de la création de chaque instance du component. 
+  Elle permet notamment d'initialiser des propriétés. */
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = "My best friend!";
+    this.createdDate = new Date();
+    this.snaps = 6;
+  }
+}
