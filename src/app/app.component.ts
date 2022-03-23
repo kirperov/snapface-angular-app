428aca8a3def923bd42a6b657eec2af5013d2d12
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-03-23 14:31:34 +0100

    conditionning with directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,28 +13,30 @@ export class AppComponent implements OnInit {
   myThirdSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'My best friend !',
-      'https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544_960_720.jpg',
-      new Date(),
-      0
-    );
+    this.mySnap = {
+      title: 'Archibald',
+      description: 'My best friend !',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544_960_720.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: "Marseille"
+    };
 
-    this.mySecondSnap = new FaceSnap(
-      'Mountain',
-      'My voyage to the mountain',
-      'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_960_720.jpg',
-      new Date(),
-      0
-    );
+    this.mySecondSnap = {
+      title: 'Mountain',
+      description: 'My voyage to the mountain',
+      imageUrl: 'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_960_720.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: "Mountains"
+    };
 
-    this.myThirdSnap = new FaceSnap(
-      'Good restaurant',
-      'My meal was good',
-      'https://cdn.pixabay.com/photo/2020/01/17/16/42/food-4773380_960_720.jpg',
-      new Date(),
-      0
-    );
+    this.myThirdSnap = {
+      title: 'Good restaurant',
+      description: 'My meal was good',
+      imageUrl: 'https://cdn.pixabay.com/photo/2020/01/17/16/42/food-4773380_960_720.jpg',
+      createdDate: new Date(),
+      snaps: 0
+    };
   }
 }
