906617fe805d91d0b5a813d511af59bc9452f252
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-03-24 11:15:34 +0100

    add directive ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,35 +8,33 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
   // Création de propriété FaceSnap et initialisaton dans ngOnInit()
-  mySnap!: FaceSnap;
-  mySecondSnap!: FaceSnap;
-  myThirdSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'My best friend !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544_960_720.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: "Marseille"
-    };
-
-    this.mySecondSnap = {
-      title: 'Mountain',
-      description: 'My voyage to the mountain',
-      imageUrl: 'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_960_720.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: "Mountains"
-    };
-
-    this.myThirdSnap = {
-      title: 'Good restaurant',
-      description: 'My meal was good',
-      imageUrl: 'https://cdn.pixabay.com/photo/2020/01/17/16/42/food-4773380_960_720.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'My best friend !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544_960_720.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: "Marseille"
+      },
+      {
+        title: 'Mountain',
+        description: 'My voyage to the mountain',
+        imageUrl: 'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_960_720.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: "Mountains"
+      },
+      {
+        title: 'Good restaurant',
+        description: 'My meal was good',
+        imageUrl: 'https://cdn.pixabay.com/photo/2020/01/17/16/42/food-4773380_960_720.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
   }
 }
