29a7c89834811237a6f225f7a86ef22170c497da
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-03-25 17:07:42 +0100

    add single face snap component

--- src/app/single-face-snap/single-face-snap.component.ts
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -1,29 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
-// Decorator (brings modifications to the class)
 @Component({
-  selector: 'app-face-snap',
-  templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
 })
-// OnInit implementé la methode
-export class FaceSnapComponent implements OnInit {
-  // Création de propriété avec le type FaceSnap pour qu'il puisse être enjecté de l'exterieur
-  @Input() faceSnap!: FaceSnap;
+export class SingleFaceSnapComponent implements OnInit {
+// Création de propriété avec le type FaceSnap pour qu'il puisse être enjecté de l'exterieur
+  faceSnap!: FaceSnap;
   // Propriétés por les données
   snapped!: boolean;
   btnText!: string; 
-  
-  constructor(private faceSnapsService: FaceSnapsService, private router: Router) { }
+
+  constructor(private faceSnapsService: FaceSnapsService, private route: ActivatedRoute) { }
   // Appelée 1 fois par instance au moment de la création (Initialisation de données en dur mais possible du serveur ou API) 
- /* La méthode  ngOnInit()  est appelée automatiquement par Angular
+  /* La méthode  ngOnInit()  est appelée automatiquement par Angular
   au moment de la création de chaque instance du component. 
   Elle permet notamment d'initialiser des propriétés. */
   ngOnInit() {
     this.snapped = false;
     this.btnText = "Oh Snap!";
+    // + transform string on number
+    const faceSnapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
   }
 
   onSnap() {
@@ -36,9 +37,5 @@ export class FaceSnapComponent implements OnInit {
       this.snapped = true;
       this.btnText = "Oops, un Snap!";
     }
-  }
-
-  onViewFaceSnap() {
-    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
   }
 }
